3d5d9cce346e6c31fb20ec855c935a0654773dfd
Aldo Gamaliel Estrada Tepec <aldoget1992@gmail.com>
2017-02-14 03:49:34 -0600

    Initial commit

---
+++ app/src/main/java/com/aget/slider/Inicio.java
@@ -0,0 +1,16 @@
+package com.aget.slider;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+/**
+ * Created by Work on 14/02/17.
+ */
+
+public class Inicio extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
